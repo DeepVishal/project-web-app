1feba62d046e00451036c3e4c6576a57cce0a1dd
Deep Vishal <dvishal23@gmail.com>
2023-08-03 01:13:09 +0800

    Self | Portfolio Update

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, OnDestroy, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export interface IExp {
-  "company": string,
-  "tenure": string,
-  "logo": string,
-  "designation": string
+  company: string,
+  tenure: string,
+  logo: string,
+  designation: string
+}
+
+export interface IProject {
+  project: string,
+  client: string;
+  desc: string
+}
+
+export interface IDetails {
+  exp: IExp[],
+  projects: IProject[]
 }
 
 @Component({
@@ -14,7 +25,10 @@ export interface IExp {
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit, OnChanges, OnDestroy {
-  public exp: IExp[] = [];
+  @Input() load: 'about' | 'experience' | 'project' = 'about';
+
+  public details: IExp[] | undefined = undefined;
+  public projectDetails: IProject[] | undefined = undefined;
   public url: string = '';
   public base: string = '';
 
@@ -23,15 +37,16 @@ export class CardComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-
+  
   }
 
   ngOnInit(): void {
     this.url = './assets/experience.json';
     this.base = './assets/'
-    this.http.get<IExp[]>(this.url).subscribe((exp) => {
-      console.log(exp);
-      this.exp = exp;
+    this.http.get<IDetails>(this.url).subscribe((details) => {
+      console.log(details);
+      this.details = details.exp;
+      this.projectDetails = details.projects;
     })
 
   }
